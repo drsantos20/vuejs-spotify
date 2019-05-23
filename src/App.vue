dcd9289981b1c16af626a1f733d119d00853cdc0
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-05-23 19:43:22 -0300

    Add web application skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app">
-    <MainHeader></MainHeader>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <!-- navbar -->
+      <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+        <div class="container">
+        <router-link to="/" class="navbar-brand">Home</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+      
+            <li class="nav-item">
+              <router-link class="nav-link" to="/blog">Blog</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/services">Services</router-link>
+            </li>
+            <li class="nav-item">
+             <router-link class="nav-link" to="/contact">contact</router-link>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+       <transition name="moveInUp">
+         <router-view/>
+       </transition>
+        
+      </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MainHeader from './components/MainHeader.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    MainHeader
-  }
+  name: 'App'
 }
 </script>
 
@@ -27,5 +47,31 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.moveInUp-enter-active{
+  animation: fadeIn 2s ease-in;
+}
+@keyframes fadeIn{
+  0%{
+    opacity: 0;
+  }
+  50%{
+    opacity: 0.5;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+.moveInUp-leave-active{
+  animation: moveInUp .3s ease-in;
+}
+@keyframes moveInUp{
+ 0%{
+  transform: translateY(0);
+ }
+  100%{
+  transform: translateY(-400px);
+ }
 }
 </style>
